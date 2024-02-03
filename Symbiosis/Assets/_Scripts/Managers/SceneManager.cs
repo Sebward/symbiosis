08154cfbad522edbae718fc4ffcf835625364b2c
Shaxiipoo <Hulse530@gmail.com>
2024-02-02 21:49:51 -0500

    Added Main Menu & Setting

--- Symbiosis/Assets/_Scripts/Managers/SceneManager.cs
+++ Symbiosis/Assets/_Scripts/Managers/SceneManager.cs
@@ -1,19 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Build.Content;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
-public class SceneManager : MonoBehaviour
+public class SceneManage : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject settings;
+    public int i_activeScene = 0;
+    public int i_menuState = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        i_menuState = 0;
+        menu.SetActive(true);
+        settings.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        i_activeScene = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void SetScene(int targetScene)
+    {
+        if (i_activeScene == 0)
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+
+    public void Quit(string s_currentEnviorment)
+    {
+        if (s_currentEnviorment == "E")
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        if (s_currentEnviorment == "B")
+        {
+            Application.Quit();
+        }
+    }
+
+    public void back()
+    {
+        if (i_menuState == 1)
+        {
+            menu.SetActive(true);
+            settings.SetActive(false);
+            i_menuState = 0;
+        }
+    }
+
+    public void Settings()
+    {
+        if (i_menuState == 0)
+        {
+            menu.SetActive(false);
+            settings.SetActive(true);
+            i_menuState = 1;
+        }
     }
 }
