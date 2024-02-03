64a11181aa6bf4464fcf17c532b3b77b1ecd9718
Shaxiipoo <Hulse530@gmail.com>
2024-02-02 20:49:59 -0500

    Frog stuff and scene manager

---
+++ Symbiosis/Assets/_Scripts/Managers/SceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
