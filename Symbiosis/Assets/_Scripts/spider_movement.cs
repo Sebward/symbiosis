07dbedb8e777221fd4c63778451183ed182c3297
ZhangTsingtao <rickzhangqt@gmail.com>
2024-02-06 13:05:00 -0500

    Spider movement

--- Symbiosis/Assets/_Scripts/spider_movement.cs
+++ Symbiosis/Assets/_Scripts/spider_movement.cs
@@ -1,72 +1,33 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
-
-public class frog_movement : MonoBehaviour
+public class spider_movement : MonoBehaviour
 {
-    [Header("Horizontal Movement")]
-    [SerializeField] private float f_move_speed = 0.2f;
+    [Header("Movement")]
+    [SerializeField] private float f_move_speed = 5.0f;
     [SerializeField] private float f_top_speed = 8.0f;
-    [SerializeField] private float f_move_damper = 1.0f;
-
-    [Header("Jump")]
-    [SerializeField] private float f_jump_speed = 10.0f;
-    [SerializeField] private float f_g = 9.8f;
+    [SerializeField] private float f_damper = 1.0f;
 
     [Header("Debug")]
     [SerializeField] private float f_horizontal_velocity = 0.0f;
     [SerializeField] private float f_vertical_velocity = 0.0f;
-    [SerializeField] private bool b_grounded = false;
+    [SerializeField] private Vector2 v2_move_velocity;
     [SerializeField] private BoxCollider2D b_collider;
-    //[SerializeField] private bool b_h_hit = false;
 
-    private void Start()
+    void Start()
     {
         b_collider = GetComponent<BoxCollider2D>();
         if (b_collider == null) b_collider = transform.AddComponent<BoxCollider2D>();
     }
+
     void Update()
     {
-        //Horizontal
-        //inputs
-        if( Input.GetKey(KeyCode.A) )
-        {
-            f_horizontal_velocity -= f_move_speed;
-            //Debug.Log(f_horizontal_velocity.ToString());
-        }
-        else if ( Input.GetKey(KeyCode.D) )
-        {
-            f_horizontal_velocity += f_move_speed;
-            //Debug.Log(f_horizontal_velocity.ToString());
-        }
-        else
-        {
-            if (Mathf.Abs(f_horizontal_velocity) > 0.0f)
-            {
-                f_horizontal_velocity = (f_horizontal_velocity > 0) ? f_horizontal_velocity - f_move_damper : f_horizontal_velocity + f_move_damper;
-                if (Mathf.Abs(f_horizontal_velocity) < f_move_damper)
-                {
-                    f_horizontal_velocity = 0.0f;
-                }
-            }
-        }
-        //horizontal speed control
-        if (Mathf.Abs(f_horizontal_velocity) > f_top_speed)
-        {
-            f_horizontal_velocity = (f_horizontal_velocity > 0) ? f_top_speed : -f_top_speed;
-        }
+        f_horizontal_velocity = Input.GetAxis("Horizontal") * f_move_speed;
+        f_vertical_velocity = Input.GetAxis("Vertical") * f_move_speed;
 
-        //Vertical
-        //input
-        if (Input.GetKeyDown(KeyCode.Space) /*&& b_grounded */){
-            f_vertical_velocity = f_jump_speed;
-        }
-        if (!b_grounded)
-        {
-            f_vertical_velocity -= f_g;
-        }
+        v2_move_velocity = new Vector2 (f_horizontal_velocity, f_vertical_velocity);
     }
     private void FixedUpdate()
     {
@@ -106,24 +67,21 @@ public class frog_movement : MonoBehaviour
         // If it hits something...
         if (h_l_hit > 0)
         {
-            f_horizontal_velocity = (f_horizontal_velocity > 0) ? f_horizontal_velocity : 0;
+            v2_move_velocity.x = (v2_move_velocity.x > 0) ? v2_move_velocity.x : 0;
         }
         if (h_r_hit > 0)
         {
-            f_horizontal_velocity = (f_horizontal_velocity < 0) ? f_horizontal_velocity : 0;
+            v2_move_velocity.x = (v2_move_velocity.x < 0) ? v2_move_velocity.x : 0;
         }
         if (v_u_hit > 0)
         {
-            f_vertical_velocity = (f_vertical_velocity > 0) ? -0.8f*f_vertical_velocity : f_vertical_velocity; 
+            v2_move_velocity.y = (v2_move_velocity.y < 0) ? v2_move_velocity.y : 0;
         }
-
         if (v_d_hit > 0)
         {
-            f_vertical_velocity = (f_vertical_velocity > 0) ? f_vertical_velocity : 0;
-            b_grounded = true;
+            v2_move_velocity.y = (v2_move_velocity.y > 0) ? v2_move_velocity.y : 0;
         }
-        else b_grounded = false;
 
-        transform.position += new Vector3(f_horizontal_velocity * Time.deltaTime, f_vertical_velocity * Time.deltaTime, 0);      
+        transform.position += new Vector3(v2_move_velocity.x * Time.deltaTime, v2_move_velocity.y * Time.deltaTime, 0);
     }
 }
