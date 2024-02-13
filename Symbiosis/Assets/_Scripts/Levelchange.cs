13da8e35aea111ac813abd27bdfbfbfbc6de78d2
RitoxicGame <bigmagicrat@gmail.com>
2024-02-13 01:03:26 -0500

    Update Levelchange.cs
    Fixed transitions to carry both players to next zone

--- Symbiosis/Assets/_Scripts/Levelchange.cs
+++ Symbiosis/Assets/_Scripts/Levelchange.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 
 public class Levelchange : MonoBehaviour
 {
-    //public int level;
-
     public GameObject go_target_location;
     private Vector2 v_player_target_location;
 
@@ -15,62 +13,32 @@ public class Levelchange : MonoBehaviour
     private Vector2 v_camera_target_location;
 
     public GameObject Camera;
-    //public GameObject Camera2;
-    //public GameObject Camera3;
-    //public GameObject Camera4;
-    //----------------------------------]
-    //----------------------------------]
+
+    private GameObject[] players = new GameObject[2];
 
     void Start()
     {
         v_player_target_location = go_target_location.transform.position;
         v_camera_target_location = camera_target_location.transform.position;
-        //Camera.SetActive(true);
-        //Camera2.SetActive(false);
-        //Camera3.SetActive(false);
-        //Camera4.SetActive(false);
+
+        players[0] = GameObject.FindWithTag("Frog");
+        players[1] = GameObject.FindWithTag("Spider");
     }
-    //--------------------------------------------------------------]
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log("Player touched loading zone");
-        if (other.gameObject.CompareTag("Player"))
-        {
-            //if (gameObject.tag == ("Forward")) //what in God's name is this ;_; ~QP
-            //{
-            //    if (level == 1)
-            //    {
-            //        GoTo(v_player_target_location, Camera1,Camera2);
-            //    }
-            //    else if (level == 2)
-            //    {
-            //        GoTo(v_player_target_location, Camera2, Camera3);
-            //    }
-            //    else if (level == 3)
-            //    {
-            //        GoTo(v_player_target_location, Camera3, Camera4);
-            //    }
-            //}
-
-            GoTo(other.gameObject, v_player_target_location, v_camera_target_location);
-        }
+        if (other.gameObject.CompareTag("Spider") || other.gameObject.CompareTag("Frog")) GoTo(v_player_target_location, v_camera_target_location);
     }
-    //--------------------------------------------------------------]
-    public void GoTo(GameObject Player, Vector2 PlayerTargetLoc, Vector2 CameraTargetLoc)
+    public void GoTo(Vector2 PlayerTargetLoc, Vector2 CameraTargetLoc)
     {
-        Player.transform.position = new Vector3(PlayerTargetLoc.x, PlayerTargetLoc.y, Player.transform.position.z);
+        //Player.transform.position = new Vector3(PlayerTargetLoc.x, PlayerTargetLoc.y, Player.transform.position.z);
+        foreach(var player in players)
+        {
+            player.transform.position = new Vector3(PlayerTargetLoc.x, PlayerTargetLoc.y, player.transform.position.z);
+        }
+
         Camera.transform.position = new Vector3(CameraTargetLoc.x, CameraTargetLoc.y, Camera.transform.position.z);
 
-        //New_Camera.SetActive(true);
-        //Old_Camera.SetActive(false);
-        //level++;
-        Debug.Log("Camera Position: " + Camera.transform.position);
+        //Debug.Log("Camera Position: " + Camera.transform.position);
     }
-    //public void SetStart()
-    //{
-    //    if( level == 1)
-    //    {
-    //        v_player_target_location = go_target_location.transform.position;
-    //    }
-    //}
 }
